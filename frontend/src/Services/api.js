a0804f3755c39cf13e57a6892840ac01aa64bcec
Vwegba Okiti <ovwegbaokiti@gmail.com>
2026-01-14 08:58:28 -0800

    Hide API key using env variables

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "f320ac9877c6c1101a85e0711680c46e";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
